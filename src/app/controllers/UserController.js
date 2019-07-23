af57446bac930dea9ca1ce017bbccb9ead6c05d3
Lauriel Mesquita <laurielmesquita@me.com>
2019-07-22 21:53:38 -0300

    Authentication Ok

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,9 +2,20 @@ import User from '../models/User'
 
 class UserController {
   async store (req, res) {
-    const user = await User.create(req.body)
+    const userExists = await User.findOne({ where: { mail: req.body.email } })
 
-    return res.json(user)
+    if (userExists) {
+      return res.status(400).json({ error: 'Usuário já existente!' })
+    }
+
+    const { id, name, email } = await User.create(req.body)
+
+    return res.json({ id, name, email })
+  }
+
+  async update (req, res) {
+    console.log(req.userId)
+    return res.json({ Ok: true })
   }
 }
 
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '250b7cd274793ed2345497c093b51831',
+  expiresIn: '7d'
+}
